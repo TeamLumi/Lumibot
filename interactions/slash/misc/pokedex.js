029c7e450a73cdfa0eb854bb974fa54953dbe9b1
fakuzatsu <abyerswilson@gmail.com>
2023-06-09 17:36:10 +0100

    also included __gamedata

--- interactions/slash/misc/pokedex.js
+++ interactions/slash/misc/pokedex.js
@@ -1,9 +1,5 @@
 const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
 const fs = require('fs');
-const path = require('path');
-const axios = require('axios');
-
-const pokemonDataPath = path.join(__dirname, '../../../gamedata/input/LumiMons.json');
 
 const typeColors = {
   Grass: '#A3B18A',
@@ -36,63 +32,6 @@ module.exports = {
         .setRequired(true)),
 
   execute(interaction) {
-    const pokemonName = interaction.options.getString('pokemon').toLowerCase();
-    const pokemonNameCapital = pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1);
-
-    fs.readFile(pokemonDataPath, 'utf8', (err, data) => {
-      if (err) {
-        console.error(err);
-        return interaction.reply('An error occurred while retrieving Pokémon data.');
-      }
-
-      const pokemonData = JSON.parse(data);
-
-      const pokemon = pokemonData[pokemonNameCapital];
-
-      if (!pokemon) {
-        const embed = new EmbedBuilder()
-          .setTitle(`Shaymin:`)
-          .setDescription(`Oops! No data found for the provided Pokemon. Run '/help pokedex' for more information about how to use this command.`);
-        return interaction.reply({ embeds: [embed] });
-      }
-
-      const description = 
-      `**Types**: ${pokemon.types.join(', ')}\n
-      **Base Stats**:\n
-      HP: ${pokemon.bs.hp}
-      AT: ${pokemon.bs.at}
-      DF: ${pokemon.bs.df}
-      SA: ${pokemon.bs.sa}
-      SD: ${pokemon.bs.sd}
-      SP: ${pokemon.bs.sp}\n
-      **Weight**: ${pokemon.weightkg} kg\n
-      **Abilities**: ${Object.values(pokemon.abilities).join(', ')}`;
-
-      axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
-      .then(response => {
-        const imageURL = response.data.sprites.front_default;
-
-        const embed = new EmbedBuilder()
-        .setTitle(pokemonNameCapital)
-        .setDescription(description.trim())
-        .setThumbnail(imageURL);
-
-        const pokemonTypes = pokemon.types;
-        if (pokemonTypes.length > 0) {
-          const primaryType = pokemonTypes[0];
-          const typeColor = typeColors[primaryType];
-          if (typeColor) {
-            embed.setColor(typeColor);
-          }
-        }
-
-        interaction.reply({ embeds: [embed] });
-      })
-      .catch(err => {
-        console.error(err);
-        interaction.reply('An error occurred while retrieving Pokemon data.');
-      })
-
-    });
-  },
+    
+    }
 };
