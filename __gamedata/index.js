528714fe96cbc2ee177d795734b5f9be407a3e66
fakuzatsu <abyerswilson@gmail.com>
2023-06-10 21:01:47 +0100

    removed trainer_info and trainer util

--- __gamedata/index.js
+++ __gamedata/index.js
@@ -27,7 +27,6 @@ const itemNames = require('./english_ss_itemname.json');
 // Custom files
 const moveEnum = require('./moveEnum.json');
 const smogonMoves = require('./smogonMoves.json');
-const trainerInfo = require('./trainer_info.json');
 
 module.exports = {
   PersonalTable,
