fab988c855fd6fb51c2699e8cd009f558eca4acb
fakuzatsu <abyerswilson@gmail.com>
2023-06-10 21:16:02 +0100

    code consistency

--- messages/defaultSelectError.js
+++ messages/defaultSelectError.js
@@ -1,9 +1,4 @@
 module.exports = {
-	/**
-	 * @description Executes when the select menu interaction could not be fetched.
-	 * @author Naman Vrati
-	 * @param {import('discord.js').SelectMenuInteraction} interaction The Interaction Object of the command.
-	 */
 
 	async execute(interaction) {
 		await interaction.reply({
--- messages/onMention.js
+++ messages/onMention.js
@@ -1,11 +1,6 @@
 const { prefix } = require("../config.json");
 
 module.exports = {
-	/**
-	 * @description Executes when the bot is pinged.
-	 * @author Naman Vrati
-	 * @param {import('discord.js').Message} message The Message Object of the command.
-	 */
 
 	async execute(message) {
 		return message.channel.send(
