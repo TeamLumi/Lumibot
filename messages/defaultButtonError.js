197bc48bec5c077579312c57f572be9ba5819838
Phuc Tran <glup3.tran@gmail.com>
2023-06-15 13:20:06 +0200

    add typings to messages

--- messages/defaultButtonError.js
+++ messages/defaultButtonError.js
@@ -1,4 +1,8 @@
 module.exports = {
+	/**
+	 * @description Executes when the button interaction could not be fetched.
+	 * @param {import('discord.js').ButtonInteraction} interaction The Interaction Object of the command.
+	 */
 	async execute(interaction) {
 		await interaction.reply({
 			content: "There was an issue while fetching this button!",
