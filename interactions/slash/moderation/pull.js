a05c1abec67a28329dc99bf3c84af305963a1d06
fakuzatsu <abyerswilson@gmail.com>
2023-07-29 13:54:52 +0100

    git pull added as a slash command

---
+++ interactions/slash/moderation/pull.js
@@ -0,0 +1,48 @@
+const {
+	AttachmentBuilder,
+	EmbedBuilder,
+	PermissionsBitField,
+	SlashCommandBuilder,
+} = require("discord.js");
+const simpleGit = require("simple-git");
+
+/**
+ * @type {import('../../../typings').SlashInteractionCommand}
+ */
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("pull")
+		.setDescription("Super Mod Command: Pulls the remote repo to the bot.")
+		.setDefaultMemberPermissions(PermissionsBitField.Flags.Administrator)
+		.setDMPermission(false),
+
+	async execute(interaction) {
+		// Check if there are pending changes in the local repository
+		const git = simpleGit();
+		const statusSummary = await git.status();
+
+		if (statusSummary.files.length > 0) {
+			await interaction.reply({
+				content:
+					"Cannot pull changes because there are pending changes in the local repository.\nTell my creator to stop tinkering with my code on the local repo.",
+				ephemeral: true,
+			});
+		} else {
+			try {
+				await git.pull();
+				await interaction.reply({
+					content:
+						"Successfully pulled changes from the remote repository. Thanks for the update!",
+					ephemeral: true,
+				});
+			} catch (error) {
+				console.log(error);
+				await interaction.reply({
+					content:
+						"Failed to pull changes from the remote repository. See the log for more info.",
+					ephemeral: true,
+				});
+			}
+		}
+	},
+};
