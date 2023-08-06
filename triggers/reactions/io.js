101a184b5cad92b3b095bad37b13433425e4be34
fakuzatsu <abyerswilson@gmail.com>
2023-08-06 19:54:03 +0100

    io fixed

--- triggers/reactions/io.js
+++ triggers/reactions/io.js
@@ -8,11 +8,7 @@ module.exports = {
 		name: ["[io]", "[randomize]", "[randomise]"],
 	},
 	execute(message, args) {
-		const embed = new EmbedBuilder()
-			.setDescription(
-				`Imposter's Ordeal is a BDSP randomizer that also allows you to edit a vast array of game functions. Requires a romfs/exefs extraction of 1.3.0 (BD if you're going to add Lumi, though SP works if you're just messing with vanilla). From there, click "add mod" to load the Luminescent romfs/exefs folders. Once you're finished editing, put the resultant "output" files into the space of your Luminescent mod.\n\nImposter's Ordeal allows you to change move attributes (types, additional effects, accuracy, etc), shop lists, route encounters, and Pokémon statistics such as their typing, BST, and wild held items.\n\nThe **randomizer function** is only compatible with versions 2.0.4+. [Here is our guide](https://www.nexusmods.com/pokemonbdsp/articles/3) on how to do it.\n\nAlways download the **latest version** from the [releases page](https://github.com/Nifyr/Imposters-Ordeal/releases). Requires .NET 5.0.`,
-			)
-			.setColor(0x000000);
+		const embed = new EmbedBuilder().setColor(0x000000);
 
 		if (message.content.toLowerCase().includes("[io]")) {
 			embed
@@ -20,6 +16,9 @@ module.exports = {
 				.setURL("https://github.com/Nifyr/Imposters-Ordeal/")
 				.setThumbnail(
 					"https://www.serebii.net/cafemix/pokemon/132-pretendeevee.png",
+				)
+				.setDescription(
+					`Imposter's Ordeal is a BDSP randomizer that also allows you to edit a vast array of game functions. Requires a romfs/exefs extraction of 1.3.0 (BD if you're going to add Lumi, though SP works if you're just messing with vanilla). From there, click "add mod" to load the Luminescent romfs/exefs folders. Once you're finished editing, put the resultant "output" files into the space of your Luminescent mod.\n\nImposter's Ordeal allows you to change move attributes (types, additional effects, accuracy, etc), shop lists, route encounters, and Pokémon statistics such as their typing, BST, and wild held items.\n\nThe **randomizer function** is only compatible with versions 2.0.4+. [Here is our guide](https://www.nexusmods.com/pokemonbdsp/articles/3) on how to do it.\n\nAlways download the **latest version** from the [releases page](https://github.com/Nifyr/Imposters-Ordeal/releases). Requires .NET 5.0.`,
 				);
 		} else {
 			embed
@@ -27,6 +26,9 @@ module.exports = {
 				.setURL("https://www.nexusmods.com/pokemonbdsp/articles/3")
 				.setThumbnail(
 					"https://cdn.discordapp.com/attachments/915474449657126932/1105083844073369621/randomize_.png",
+				)
+				.setDescription(
+					"Check this page out for instructions and necessary links involved for randomizing Pokémon Luminescent Platinum!\n\nPlease note that changing the starter selection is not possible due to the bizarro fact that those are hard-coded, and any hard-coding changes will break Lumi.",
 				);
 		}
 
