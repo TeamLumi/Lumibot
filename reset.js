7fd44458334da037735fbd704c4afb8c4a8c3a54
fakuzatsu <abyerswilson@gmail.com>
2023-08-07 10:51:24 +0100

    reset.js changed and creating jest files

--- reset.js
+++ reset.js
@@ -1,5 +1,24 @@
 const { REST, Routes } = require("discord.js");
-const { token, client_id, test_guild_id } = require("./config.json");
+const {
+	token_dev,
+	token_prod,
+	client_id_dev,
+	client_id_prod,
+	test_guild_id,
+} = require("./config.json");
+
+let token = [];
+let client_id = [];
+
+if (process.env.NODE_ENV === "production") {
+	console.log("Running in production mode");
+	token = token_prod;
+	client_id = client_id_prod;
+} else {
+	console.log("Running in development mode");
+	token = token_dev;
+	client_id = client_id_dev;
+}
 
 const rest = new REST({ version: "9" }).setToken(token);
 
