0c7719a6996eaee37170a8e6e7568dd36044d36e
Eevenin <73308769+biomagnet@users.noreply.github.com>
2023-08-13 14:31:53 -0500

    Update status bar.js

--- triggers/reactions/status bar.js
+++ triggers/reactions/status bar.js
@@ -5,7 +5,7 @@ const { EmbedBuilder, AttachmentBuilder } = require("discord.js");
  */
 module.exports = {
 	data: {
-		name: ["[status bar]", "[status]", "[1.3.0]"],
+		name: ["[status bar]", "[status]", "[1.3.0]",],
 	},
 	execute(message, args) {
 		if (
