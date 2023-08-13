f301dac9a2703f959a46bdb4d8bcbb05d9ff045a
Eevenin <73308769+biomagnet@users.noreply.github.com>
2023-08-13 01:29:48 -0500

    Update special.js to separate tags

--- triggers/reactions/special.js
+++ triggers/reactions/special.js
@@ -29,15 +29,35 @@ module.exports = {
 		if (message.content.toLowerCase().includes("[encounters]")) {
 			embed.setURL("https://luminescent.team/docs/category/special-events");
 		} else if (message.content.toLowerCase().includes("[gifts]")) {
-			embed.setURL("https://luminescent.team/docs/special-events/gifts");
+			embed.setTitle("Gifted Pokémon")
+			.setURL("https://luminescent.team/docs/special-events/gifts")
+			.setDescription(
+			"See what gifts you can receive in Luminescent Platinum."
+			);
 		} else if (message.content.toLowerCase().includes("[static]")) {
-			embed.setURL("https://luminescent.team/docs/special-events/static");
+			embed.setTitle("Static Encounters")
+			.setURL("https://luminescent.team/docs/special-events/static")
+			.setDescription(
+			"See what static Pokémon you can encounter on the overworld in Luminescent Platinum."
+			);
 		} else if (message.content.toLowerCase().includes("[trades]")) {
-			embed.setURL("https://luminescent.team/docs/special-events/trade");
+			embed.setTitle("Traded Pokémon")
+			.setURL("https://luminescent.team/docs/special-events/trade")
+			.setDescription(
+			"All available trades in the game."
+			);
 		} else if (message.content.toLowerCase().includes("[trade]")) {
-			embed.setURL("https://luminescent.team/docs/special-events/trade");
+			embed.setTitle("Traded Pokémon")
+			.setURL("https://luminescent.team/docs/special-events/trade")
+			.setDescription(
+			"All available trades in the game."
+			);
 		} else {
-			embed.setURL("https://luminescent.team/docs/special-events/legendaries");
+			embed.setTitle("Legendary Encounters")
+			.setURL("https://luminescent.team/docs/special-events/legendaries")
+			.setDescription(
+			"See how to find and capture all legendary Pokémon in Luminescent Platinum."
+			);
 		}
 
 		message.channel.send({ embeds: [embed] });
