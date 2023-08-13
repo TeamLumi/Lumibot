5710b1cbe12b993614758b21a5a825b9b3316b45
fakuzatsu <abyerswilson@gmail.com>
2023-08-13 21:31:55 +0100

    items now returns the correct link

--- triggers/reactions/items.js
+++ triggers/reactions/items.js
@@ -13,7 +13,7 @@ module.exports = {
 				"https://cdn.discordapp.com/attachments/1115345759496323173/1115681769887387748/items.png",
 			)
 			.setTitle("Items & TMs")
-			.setURL("https://luminescent.team/docs/features")
+			.setURL("https://luminescent.team/docs/items")
 			.setDescription(
 				"List of our items & TM locations. Currently copied from Renegade Platinum's list, and being updated to match Luminescent Platinum's.\n\nPlease report any issues or corrections to https://discord.com/channels/912508046159261728/1073098396774182972",
 			)
