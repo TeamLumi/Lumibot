6882dbb98fcf464ace4a2da14a672a3f35d1d5eb
Eevenin <73308769+biomagnet@users.noreply.github.com>
2023-10-11 16:41:51 -0500

    Update io.js

--- triggers/reactions/io.js
+++ triggers/reactions/io.js
@@ -30,7 +30,7 @@ module.exports = {
 				.setDescription(
 					`Imposter's Ordeal is a BDSP randomizer that also allows you to edit a vast array of game functions. Requires a romfs/exefs extraction of 1.3.0 (BD if you're going to add Lumi, though SP works if you're just messing with vanilla). From there, click "add mod" to load the Luminescent romfs/exefs folders. Once you're finished editing, put the resultant "output" files into the space of your Luminescent mod.\n\nImposter's Ordeal allows you to change move attributes (types, additional effects, accuracy, etc), shop lists, route encounters, and Pokémon statistics such as their typing, BST, and wild held items.\n\nThe **randomizer function** is only compatible with versions 2.0.4+. [Here is our guide](https://www.nexusmods.com/pokemonbdsp/articles/3) on how to do it.\n\nAlways download the **latest version** from the [releases page](https://github.com/Nifyr/Imposters-Ordeal/releases). Requires .NET 5.0.`,
 				);
-		if (message.content.toLowerCase().includes("[imposter's ordeal]")) {
+		else if (message.content.toLowerCase().includes("[imposter's ordeal]")) {
 			embed
 				.setTitle("Imposter's Ordeal")
 				.setURL("https://github.com/Nifyr/Imposters-Ordeal/")
@@ -40,7 +40,7 @@ module.exports = {
 				.setDescription(
 					`Imposter's Ordeal is a BDSP randomizer that also allows you to edit a vast array of game functions. Requires a romfs/exefs extraction of 1.3.0 (BD if you're going to add Lumi, though SP works if you're just messing with vanilla). From there, click "add mod" to load the Luminescent romfs/exefs folders. Once you're finished editing, put the resultant "output" files into the space of your Luminescent mod.\n\nImposter's Ordeal allows you to change move attributes (types, additional effects, accuracy, etc), shop lists, route encounters, and Pokémon statistics such as their typing, BST, and wild held items.\n\nThe **randomizer function** is only compatible with versions 2.0.4+. [Here is our guide](https://www.nexusmods.com/pokemonbdsp/articles/3) on how to do it.\n\nAlways download the **latest version** from the [releases page](https://github.com/Nifyr/Imposters-Ordeal/releases). Requires .NET 5.0.`,
 				);
-		if (message.content.toLowerCase().includes("[imposters ordeal]")) {
+		else if (message.content.toLowerCase().includes("[imposters ordeal]")) {
 			embed
 				.setTitle("Imposter's Ordeal")
 				.setURL("https://github.com/Nifyr/Imposters-Ordeal/")
