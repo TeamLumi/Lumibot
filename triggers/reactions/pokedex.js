e7a016243f5b4bba9a2cca0ddc63c73244fdcfd2
Eevenin <73308769+biomagnet@users.noreply.github.com>
2023-10-11 19:38:08 -0500

    Update pokedex.js

--- triggers/reactions/pokedex.js
+++ triggers/reactions/pokedex.js
@@ -4,22 +4,18 @@ const { EmbedBuilder, AttachmentBuilder } = require("discord.js");
  * @type {import('../../typings').TriggerCommand}
  */
 module.exports = {
-	data: {
-		name: ["[pokedex]", 
-           "[dex]",
-          ],
-	},
-	execute(message, args) {
-              const embed = new EmbedBuilder()
-          		.setAuthor({
-              			name: "Team Lumi",
-              			})
-          		.setTitle("Pokédex")
-          		.setURL("https://luminescent.team/pokedex")
-          		.setDescription("Check out our online Pokédex! Includes movesets, BSTs, types and wild held items. Its information is taken directly from the game files themselves, so everything will be 100% accurate to what is in game, even if the game itself contains errors.\n\nPlease note that it currently does not include Pokémon locations. We ask that you continue to use <#1139732466857418872> in the meanwhile.")
-          		.setThumbnail("https://cdn.discordapp.com/attachments/1115345759496323173/1115682731884560485/gremlin.png");
-    			.setColor(0x000000);
-
-		message.channel.send({ embeds: [embed] });
-  },
+    data: {
+        name: ["[name]", "[alias]"],
+    },
+    execute(message, args) {
+      const embed = new EmbedBuilder()
+  .setAuthor({
+    name: "Team Lumi",
+  })
+  .setTitle("Pokédex")
+  .setURL("https://luminescent.team/pokedex")
+  .setDescription("Check out our online Pokédex! Includes movesets, BSTs, types and wild held items. Its information is taken directly from the game files themselves, so everything will be 100% accurate to what is in game, even if the game itself contains errors.\n\nPlease note that it currently does not include Pokémon locations. We ask that you continue to use <#1139732466857418872> in the meanwhile.")
+  .setThumbnail("https://cdn.discordapp.com/attachments/1115345759496323173/1115682731884560485/gremlin.png");
+      message.channel.send({ embeds: [embed] });
+    },
 };
