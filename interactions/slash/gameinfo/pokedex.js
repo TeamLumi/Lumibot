7bd3fd2c44ba28419509259f17c889b13a326418
fakuzatsu <abyerswilson@gmail.com>
2023-10-26 20:24:20 +0100

    removed extra unecessary components

--- interactions/slash/gameinfo/pokedex.js
+++ interactions/slash/gameinfo/pokedex.js
@@ -6,12 +6,7 @@ const {
 const {
 	getPokemonIdFromDisplayName,
 	getPokemonInfo,
-	generateMovesViaLearnset,
-	getEvolutionTree,
-	getPokemonDisplayName,
-	getEvolutionMethodDetail,
 } = require("../../../dex/index.js");
-const { CanvasRenderService } = require("chartjs-node-canvas");
 const { locationMode } = require("../../../helpers/pokedexLocation.js");
 const { evolutionMode } = require("../../../helpers/pokedexEvolution.js");
 const { learnsetMode } = require("../../../helpers/pokedexLearnset.js");
@@ -20,52 +15,6 @@ const {
 	createTextVisualization,
 	statisticsMode,
 } = require("../../../helpers/pokedexStatistics.js");
-
-// Array for pokemon types to set colours.
-// prettier-ignore
-const typeColors = {
-	Grass: "#09B051",
-	Fire: "#EE8130",
-	Water: "#6390F0",
-	Electric: "#F7D02C",
-	Ice: "#96D9D6",
-	Fighting: "#C22E28",
-	Poison: "#A33EA1",
-	Ground: "#E2BF65",
-	Flying: "#A98FF3",
-	Psychic: "#F95587",
-	Bug: "#A6B91A",
-	Rock: "#B6A136",
-	Ghost: "#735797",
-	Dragon: "#6F35FC",
-	Dark: "#705746",
-	Steel: "#B7B7CE",
-	Fairy: "#D685AD",
-	Normal: "#A8A77A",
-};
-
-// Array for pokemon types to set icons.
-// prettier-ignore
-const typeIcons = {
-	Grass: "<:t_grass:1117063031579488370>",
-	Fire: "<:t_fire:1117063764487962624>",
-	Water: "<:t_water:1117063766308298772>",
-	Electric: "<:t_electric:1117063036268711957>",
-	Ice: "<:t_ice:1117062636861927505>",
-	Fighting: "<:t_fighting:1117063035224334426>",
-	Poison: "<:t_poison:1117062634219524146>",
-	Ground: "<:t_ground:1117062637566570538>",
-	Flying: "<:t_flying:1117063032644845680>",
-	Psychic: "<:t_psychic:1117062633191919657>",
-	Bug: "<:t_bug:1117062630553702431>",
-	Rock: "<:t_rock:1117062629282816061>",
-	Ghost: "<:t_ghost:1117062639420452874>",
-	Dragon: "<:t_dragon:1117062647439949875>",
-	Dark: "<:t_dark:1117063037858353162>",
-	Steel: "<:t_steel:1117062632172683414>",
-	Fairy: "<:t_fairy:1117062642964635698>",
-	Normal: "<:t_normal:1117062635817554010>",
-};
 
 /**
  * @type {import('../../../typings').SlashInteractionCommand}
@@ -178,41 +127,44 @@ module.exports = {
 			return interaction.reply({ embeds: [embed] });
 		}
 
-		if (mode === "location") {
-			embed = locationMode(name, imageLnk, type1, monsID, isValid, interaction);
-			return interaction.reply({ embeds: [embed] });
-		} else if (mode === "evolution") {
-			embed = evolutionMode(name, imageLnk, type1, monsID, isValid);
-			return interaction.reply({ embeds: [embed] });
-		} else if (mode === "learnset") {
-			embed = learnsetMode(name, imageLnk, type1, monsID, isValid);
-			return interaction.reply({ embeds: [embed] });
-		} else {
-			const embed = statisticsMode(
-				name,
-				imageLnk,
-				ability1,
-				ability2,
-				abilityH,
-				baseStatsTotal,
-				type1,
-				type2,
-				isValid,
-				genderDecimalValue,
-			);
-			if (visualization === "graph") {
-				const image = await createGraphVisualization(pokemonInfo, BackupInfo);
-				const attachment = new AttachmentBuilder(image, { name: "chart.png" });
-				embed.setImage("attachment://chart.png");
-				interaction.reply({ embeds: [embed], files: [attachment] });
-			} else {
-				const textInfo = createTextVisualization(pokemonInfo, BackupInfo);
-				embed.addFields({
-					name: "**Base Stats:**",
-					value: textInfo,
-				});
-				return interaction.reply({ embeds: [embed] });
-			}
+		switch (mode) {
+			case "location":
+				embed = locationMode(name, imageLnk, type1, monsID, isValid, interaction);
+				break;
+			case "evolution":
+				embed = evolutionMode(name, imageLnk, type1, monsID, isValid);
+				break;
+			case "learnset":
+				embed = learnsetMode(name, imageLnk, type1, monsID, isValid);
+				break;
+			default:
+				embed = statisticsMode(
+					name,
+					imageLnk,
+					ability1,
+					ability2,
+					abilityH,
+					baseStatsTotal,
+					type1,
+					type2,
+					isValid,
+					genderDecimalValue
+				);
+				if (visualization === "graph") {
+					const image = await createGraphVisualization(pokemonInfo, BackupInfo);
+					const attachment = new AttachmentBuilder(image, { name: "chart.png" });
+					embed.setImage("attachment://chart.png");
+					interaction.reply({ embeds: [embed], files: [attachment] });
+					return;
+				} else {
+					const textInfo = createTextVisualization(pokemonInfo, BackupInfo);
+					embed.addFields({
+						name: "**Base Stats:**",
+						value: textInfo,
+					});
+				}
 		}
+		
+		interaction.reply({ embeds: [embed] });
 	},
 };
