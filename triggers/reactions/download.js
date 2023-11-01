7ef8a3fea15345ba28211bfff01cf98884b31743
Eevenin <73308769+biomagnet@users.noreply.github.com>
2023-11-01 18:09:44 -0500

    Update download.js

--- triggers/reactions/download.js
+++ triggers/reactions/download.js
@@ -5,7 +5,7 @@ const { EmbedBuilder, AttachmentBuilder } = require("discord.js");
  */
 module.exports = {
 	data: {
-		name: ["[download]"],
+		name: ["[download]", "[nexus]"],
 	},
 	execute(message, args) {
 		const embed = new EmbedBuilder()
