c75feb692e587c34bf967b49b9a0ddb246fa1e1b
Eevenin <73308769+biomagnet@users.noreply.github.com>
2023-11-06 12:46:34 -0600

    Update tracker.js

--- triggers/reactions/tracker.js
+++ triggers/reactions/tracker.js
@@ -5,7 +5,7 @@ const { EmbedBuilder, AttachmentBuilder } = require("discord.js");
  */
 module.exports = {
 	data: {
-		name: ["[tracker]"],
+		name: ["[tracker]", "[nuzlocke]"],
 	},
 	execute(message, args) {
 		const embed = new EmbedBuilder()
