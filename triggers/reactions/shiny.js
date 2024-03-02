06d77d52bae3a274c2301231114f73e5c11e7c60
Leoparodo <85522623+Leoparodo@users.noreply.github.com>
2024-03-02 15:36:10 -0500

    Update shiny.js
    Fixed the image link

--- triggers/reactions/shiny.js
+++ triggers/reactions/shiny.js
@@ -18,7 +18,7 @@ module.exports = {
 				"Luminescent Platinum's shiny rate is 8/4096 (1/512). This means that the shiny charm (+2) brings it to 10/4096, and the Masuda Method (+6) brings it to 14/4096. All together, you can bring the shiny rate to 16/4096.\n\nThis compounds with [radar chaining](https://cdn.discordapp.com/attachments/917239249747738624/1167491975231066183/image.png?ex=654e52a1&is=653bdda1&hm=8043bff359b11850c02bf8bd5ada206ef9bafe326f33ceac948ad7d0738d5a87&), adding one roll for each tier you achieve. (Credit to *chee* for the math/image.)\n\n***Nothing*** is shiny locked in Luminescent Platinum! For gifts, you must get to their Pokédex entry page to check their shininess.\n\nWe additionally offer a [trade](https://luminescent.team/docs/special-events/trade) for a Masuda Method 6IV Ditto in the game.",
 			)
 			.setThumbnail(
-				"https://cdn.discordapp.com/attachments/1115345759496323173/1115682729762242570/features.png",
+				"https://cdn.discordapp.com/attachments/1115345759496323173/1213585596761505895/radarodds.png?ex=65f602a9&is=65e38da9&hm=0d7b8c639d19a59ad13973a90167aec85e35b5ac392a820d31f40a178b64be3a&",
 			)
 			.setColor("#00b0f4");
 		message.channel.send({ embeds: [embed] });
