96a856ecf91333af5247a32a625f40ff5f7e0ac8
Eevenin <73308769+biomagnet@users.noreply.github.com>
2024-04-23 12:40:17 -0500

    Update pokedex.js

--- triggers/reactions/pokedex.js
+++ triggers/reactions/pokedex.js
@@ -16,7 +16,7 @@ module.exports = {
   })
   .setTitle("Pokédex")
   .setURL("https://luminescent.team/pokedex")
-  .setDescription("Check out our online Pokédex! Includes movesets, BSTs, types and wild held items. Its information is taken directly from the game files themselves, so everything will be 100% accurate to what is in game, even if the game itself contains errors.\n\nPlease note that it currently does not include Pokémon locations. We ask that you continue to use <#1139732466857418872> in the meanwhile.")
+  .setDescription("Check out our online Pokédex! Includes movesets, BSTs, types and wild held items. Its information is taken directly from the game files themselves, so everything will be 100% accurate to what is in game, even if the game itself contains errors.")
   .setThumbnail("https://cdn.discordapp.com/attachments/1115345759496323173/1115682731884560485/gremlin.png");
       message.channel.send({ embeds: [embed] });
     },
