2f58d61a301ebc180c2f94a2f6d7ca33bb9b3de7
fakuzatsu <118256341+fakuzatsu@users.noreply.github.com>
2024-09-17 10:35:32 +0100

    fix syntax error

--- triggers/reactions/relumi.js
+++ triggers/reactions/relumi.js
@@ -1,4 +1,4 @@
-const { EmbedBuilder, AttachmentBuilder } = require("discord.js");
+const { EmbedBuilder } = require("discord.js");
 
 /**
  * @type {import('../../typings').TriggerCommand}
@@ -7,17 +7,15 @@ module.exports = {
 	data: {
 		name: ["[relumi]", "[reilluminated]", "[3.0]"],
 	},
-	execute(message, args) {
+	execute(message) {
 		const embed = new EmbedBuilder()
-			.setAuthor({
-				name: "Team Lumi",
-			})
-			.setThumbnail(
-				"https://archives.bulbagarden.net/media/upload/8/84/0133Eevee-Partner.png",
-			)
+			.setAuthor("Team Lumi")
+			.setThumbnail("https://archives.bulbagarden.net/media/upload/8/84/0133Eevee-Partner.png")
 			.setTitle("Re:Illuminated Platinum")
 			.setDescription(
-				"What was was previously just called "Natdex" or "3.0" is now *Re:Illuminated Platinum*. At launch, it will include all Pokémon up to Legends Arceus (full gen 8) with gen 9 coming later on. It will also include extra story, all fights redone to include the new 'mons, expanded legendary quests (3.1+) and many other quality of life features that we are unable to do with the backend used in 2.0F/Luminescent Platinum.\n\nSaves will not be compatible between 2.0 and 3.0 because of how we will be changing the backend engine adjustments.\n\nSince this is a volunteer project with massive amounts of work still to go, we have no ETA when it will be available, and ask for your patience.",
+				`What was previously called "Natdex" or "3.0" is now *Re:Illuminated Platinum*. At launch, it will include all Pokémon up to Legends Arceus (full Gen 8) with Gen 9 coming later. It will also feature extra story content, redone fights with new Pokémon, expanded legendary quests (3.1+), and many quality-of-life improvements that weren't feasible with the backend used in 2.0F/Luminescent Platinum.\n\n` +
+				`Saves will not be compatible between 2.0 and 3.0 due to changes in the backend engine.\n\n` +
+				`As this is a volunteer project with significant work remaining, we do not have an ETA for its release and appreciate your patience.`
 			)
 			.setColor(0x00b0f4);
 
