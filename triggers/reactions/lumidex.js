060ee19bd6fcd1c679f0a5402ef254983bceb810
Eevenin <73308769+biomagnet@users.noreply.github.com>
2024-09-16 22:34:15 -0500

    Update lumidex.js

--- triggers/reactions/lumidex.js
+++ triggers/reactions/lumidex.js
@@ -17,10 +17,13 @@ module.exports = {
 			)
 			.setTitle("Luminescent Platinum Full 'Dex")
 			.setDescription(
-				"There are 510 species of Pokémon currently obtainable in Luminescent Platinum. These include the standard 493 Gen 1-4 Pokemon, along with the Litwick line, Sylveon, Obstagoon, Perrserker, Cursola, Sirfetch’d, Mr. Rime, Wyrdeer, Kleavor, Ursaluna, Sneasler, Overqwil, and the Tinkatink line. Also included are their regional pre-evolutions forms, though they aren’t required to complete the national dex. (The Pikipek line technically also exists in the backend if you randomise or pkhex.)\n\n
-
-In our upcoming romhack, Re:Illuminated, we will be expanding the National Pokédex to every Pokemon from Generations 1-8. That will be 905 Pokémon total, with Generation 9 planned to be added at a later date.",
+				"There are 510 species of Pokémon currently obtainable in Luminescent Platinum. These include the standard 493 Gen 1-4 Pokemon, along with the Litwick line, Sylveon, Obstagoon, Perrserker, Cursola, Sirfetch’d, Mr. Rime, Wyrdeer, Kleavor, Ursaluna, Sneasler, Overqwil, and the Tinkatink line. Also included are their regional pre-evolutions forms, though they aren’t required to complete the national dex. (The Pikipek line technically also exists in the backend if you randomise or pkhex.)\n\nIn our upcoming romhack, Re:Illuminated, we will be expanding the National Pokédex to every Pokemon from Generations 1-8. That will be 905 Pokémon total, with Generation 9 planned to be added at a later date.",
 			)
+
+					if (message.content.toLowerCase().includes("[lumihex]"))
+			embed.setDescription(
+				`There are 510 species of Pokémon currently obtainable in Luminescent Platinum. These include the standard 493 Gen 1-4 Pokemon, along with the Litwick line, Sylveon, Obstagoon, Perrserker, Cursola, Sirfetch’d, Mr. Rime, Wyrdeer, Kleavor, Ursaluna, Sneasler, Overqwil, and the Tinkatink line. Also included are their regional pre-evolutions forms, though they aren’t required to complete the national dex. (The Pikipek line technically also exists in the backend if you randomise or pkhex.)\n\nIn our upcoming romhack, Re:Illuminated, we will be expanding the National Pokédex to every Pokemon from Generations 1-8. That will be 905 Pokémon total, with Generation 9 planned to be added at a later date.\n\nIf you were looking for our PkHex fork, try [lumihex] instead.`,
+			);
 			.setColor(0x00b0f4);
 
 		message.channel.send({ embeds: [embed] });
