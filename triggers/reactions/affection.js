752126f12491c546ae1ca75a7c92e7c2ad6e777a
Eevenin <73308769+biomagnet@users.noreply.github.com>
2024-10-06 03:38:31 -0500

    Update affection.js

--- triggers/reactions/affection.js
+++ triggers/reactions/affection.js
@@ -18,7 +18,7 @@ module.exports = {
 			.setTitle("Affection")
 			.setURL("https://luminescent.team/docs/faq")
 			.setDescription(
-				`Among our ptions available to toggle on or off in Luminescent Platinum, we allow players to turn "affection" off at a Pokémon Center's computer.\n\n[Affection](https://bulbapedia.bulbagarden.net/wiki/Affection) is the "[Pokémon] held on so you wouldn't be sad!" mechanic. It scales on the same number as friendship/happiness, but will not affect other mechanics related to friendship. This includes Sylveon's evolution, despite previous generations specifically being tied to the affection mechanic. It's friendship now.`,
+				`Among the options available to toggle on or off in Luminescent Platinum, we allow players to turn "affection" off at a Pokémon Center's computer.\n\n[Affection](https://bulbapedia.bulbagarden.net/wiki/Affection) is the "[Pokémon] held on so you wouldn't be sad!" mechanic. It scales on the same number as friendship/happiness, but will not affect other mechanics related to friendship. This includes Sylveon's evolution, despite previous generations specifically being tied to the affection mechanic. It's friendship now.`,
 			)
 			.setColor(0x000000);
 
