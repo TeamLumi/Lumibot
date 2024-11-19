9969f8f0bff23d2de3502dc4247c90f533e84148
Leoparodo <85522623+Leoparodo@users.noreply.github.com>
2024-11-19 16:29:09 -0500

    Update relumi.js
    Added gen 9 evolution stuff

--- triggers/reactions/relumi.js
+++ triggers/reactions/relumi.js
@@ -15,7 +15,7 @@ module.exports = {
 			.setThumbnail("https://archives.bulbagarden.net/media/upload/8/84/0133Eevee-Partner.png")
 			.setTitle("Re:Illuminated Platinum")
 			.setDescription(
-				`What was previously called "Natdex" or "3.0" is now *Re:Illuminated Platinum*. At launch, it will include all Pokémon up to Legends Arceus (full Gen 8) with Gen 9 coming later. It will also feature extra story content, redone fights with new Pokémon, expanded legendary quests (3.1+), and many quality-of-life improvements that weren't feasible with the backend used in 2.0F/Luminescent Platinum.\n\n` +
+				`What was previously called "Natdex" or "3.0" is now *Re:Illuminated Platinum*. At launch, it will include all Pokémon up to Legends Arceus (full Gen 8), as well as evolution lines that got extended in Gen 9, with the rest of Gen 9 coming later. It will also feature extra story content, redone fights with new Pokémon, expanded legendary quests (3.1+), and many quality-of-life improvements that weren't feasible with the backend used in 2.0F/Luminescent Platinum.\n\n` +
 				`Saves will not be compatible between 2.0 and 3.0 due to changes in the backend engine.\n\n` +
 				`As this is a volunteer project with significant work remaining, we do not have an ETA for its release and appreciate your patience.`
 			)
