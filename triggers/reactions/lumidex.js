8509b74b4f1bb5400849ba347c25dd982c5a729b
Eevenin <73308769+biomagnet@users.noreply.github.com>
2025-06-10 15:13:18 -0500

    Update lumidex.js

--- triggers/reactions/lumidex.js
+++ triggers/reactions/lumidex.js
@@ -8,7 +8,7 @@ module.exports = {
 		name: ["[lumidex]", "[species]"],
 	},
 	execute(message, args) {
-		const baseDescription = `There are 510 species of Pokémon currently obtainable in Luminescent Platinum. These include the standard 493 Gen 1-4 Pokémon, along with the Litwick line, Sylveon, Obstagoon, Perrserker, Cursola, Sirfetch’d, Mr. Rime, Wyrdeer, Kleavor, Ursaluna, Sneasler, Overqwil, and the Tinkatink line. Also included are their regional pre-evolutions forms, though they aren’t required to complete the national dex. (The Pikipek line technically also exists in the backend if you randomise or pkhex.)\n\nIn our upcoming romhack, Re:Illuminated, we will be expanding the National Pokédex to every Pokémon from Generations 1-8. That will be 905 Pokémon total, with Generation 9 planned to be added at a later date.`;
+		const baseDescription = `There are 513 species of Pokémon currently obtainable in Luminescent Platinum. These include the standard 493 Gen 1-4 Pokémon, along with the Litwick line, Sylveon, the Pikipek line, Obstagoon, Perrserker, Cursola, Sirfetch’d, Mr. Rime, Wyrdeer, Kleavor, Ursaluna, Sneasler, Overqwil, and the Tinkatink line. Also included are their regional pre-evolutions forms, though they aren’t required to complete the national dex. \n\nIn our upcoming romhack, Re:Illuminated, we will be expanding the National Pokédex to every Pokémon from Generations 1-8. That will be 905 Pokémon total, with Generation 9 planned to be added at a later date.`;
 		const lumihexDescription = `${baseDescription}\n\nIf you were looking for our PkHex fork, try [lumihex] instead.`;
 
 		const embed = new EmbedBuilder()
