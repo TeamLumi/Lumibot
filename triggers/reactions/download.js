239eb4620ca8a139cfdf69df8875ce36d6b87677
Eevenin <73308769+biomagnet@users.noreply.github.com>
2026-01-27 12:54:07 -0600

    Update download.js
    Adding SP to the "provide your own hack" bit.

--- triggers/reactions/download.js
+++ triggers/reactions/download.js
@@ -18,7 +18,7 @@ module.exports = {
 			.setTitle("Download Luminescent Platinum")
 			.setURL("https://www.nexusmods.com/pokemonbdsp/mods/1")
 			.setDescription(
-				`Check out our [Nexus Mods page](https://www.nexusmods.com/pokemonbdsp/mods/1) to download our mod!\n\n**You must provide your own dump of Brilliant Diamond 1.3.0** from a hacked Switch to work with Luminescent Platinum; asking anybody to provide download links is piracy and an instant ban.`,
+				`Check out our [Nexus Mods page](https://www.nexusmods.com/pokemonbdsp/mods/1) to download our mod!\n\n**You must provide your own dump of Brilliant Diamond or Shining Pearl 1.3.0** from a hacked Switch to work with Luminescent Platinum; asking anybody to provide download links is piracy and an instant ban.`,
 			)
 			.setColor(0xadd8e6);
 
