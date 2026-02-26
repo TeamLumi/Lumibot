62a5d785027bfeb868be44965068a70503074431
Leoparodo <85522623+Leoparodo@users.noreply.github.com>
2026-02-26 16:01:37 -0500

    Update URL to latest PkHex release instead of overall github page
    people keep downloading code instead of the actual release

--- triggers/reactions/pkHex.js
+++ triggers/reactions/pkHex.js
@@ -16,7 +16,7 @@ module.exports = {
 				"https://archives.bulbagarden.net/media/upload/b/bf/133Eevee_Smile.png",
 			)
 			.setTitle("PkHex Luminescent / LumiHex")
-			.setURL("https://github.com/TalonSabre/PKLumiHeX")
+			.setURL("https://github.com/TalonSabre/PKLumiHex/releases/latest")
 			.setDescription(
 				"Luminescent Platinum requires a special fork of PkHex due to our save structure. Please always use the most current release to make any general edits you require, and always back up saves before editing them to avoid any potential issues.\n\nPkHex-Lumi is still in beta, so please report https://discord.com/channels/912508046159261728/1019653700169502750 if it has not been acknowledged on the github's readme or releases page. Currently only usable for inserting Pokémon, editing their stats, and adding items. If you try to insert a 'mon that is not supported in Luminescent Platinum, you'll end up with a ditto.\n\n**Note:** we do provide things such as Infinite Rare Candies, an EV trainer, farmable ability items, mints or bottlecaps, and the ability to hyper-train as soon as you have the bottlecaps, directly in the romhack itself if you would like a more natural approach.",
 			)
