f354de0beabaebdfad5f9a2ed925f36790cdbd26
Zatsu <118256341+fakuzatsu@users.noreply.github.com>
2026-03-13 17:04:54 +0000

    Rework 2 Electric Boogaloo

--- triggers/reactions/io.js
+++ triggers/reactions/io.js
@@ -30,7 +30,7 @@ module.exports = {
 					"https://www.serebii.net/cafemix/pokemon/132-pretendeevee.png",
 				)
 				.setDescription(
-					`Imposter's Ordeal is a BDSP randomizer that also allows you to edit a vast array of game functions, such as:\nMove attributes (types, additional effects, accuracy, etc), shop lists, route encounters, and Pokémon statistics such as their typing, BST, and wild held items, Trainer team composition and much more.\n\nIt requires a romfs/exefs extraction of BDSP 1.3.0 to initially startup the program. After it opens, from there, click "add mod" to load the modded Luminescent romfs/exefs folders. Once you're finished editing, put the resultant "output" files into the space of your Luminescent mod.\n\nAlways download the latest version from the releases page. Requires .NET 5.0.`,
+					`Imposter's Ordeal is a BDSP randomizer that also allows you to edit a vast array of game functions, such as:\nMove attributes (types, additional effects, accuracy, etc), shop lists, route encounters, and Pokémon statistics such as their typing, BST, and wild held items, Trainer team composition and much more.\n\nIt requires a romfs/exefs extraction of BDSP 1.3.0 to initially startup the program. After it opens, from there, click "add mod" to load the modded Luminescent romfs/exefs folders. Once you're finished editing, hit export to create your edited files to the Output folder, and to install replace your unedited Luminescent mod, with the now edited Luminescent files from the "Output" folder it generated for you.\n\nAlways download the latest version from the [releases page](https://github.com/Nifyr/Imposters-Ordeal/releases). Requires .NET 5.0.`,
 				);
 		else if (message.content.toLowerCase().includes("[imposter's ordeal]"))
 			embed
@@ -40,7 +40,7 @@ module.exports = {
 					"https://www.serebii.net/cafemix/pokemon/132-pretendeevee.png",
 				)
 				.setDescription(
-					`Imposter's Ordeal is a BDSP randomizer that also allows you to edit a vast array of game functions, such as:\nMove attributes (types, additional effects, accuracy, etc), shop lists, route encounters, and Pokémon statistics such as their typing, BST, and wild held items, Trainer team composition and much more.\n\nIt requires a romfs/exefs extraction of BDSP 1.3.0 to initially startup the program. After it opens, from there, click "add mod" to load the modded Luminescent romfs/exefs folders. Once you're finished editing, put the resultant "output" files into the space of your Luminescent mod.\n\nAlways download the latest version from the releases page. Requires .NET 5.0.`,
+					`Imposter's Ordeal is a BDSP randomizer that also allows you to edit a vast array of game functions, such as:\nMove attributes (types, additional effects, accuracy, etc), shop lists, route encounters, and Pokémon statistics such as their typing, BST, and wild held items, Trainer team composition and much more.\n\nIt requires a romfs/exefs extraction of BDSP 1.3.0 to initially startup the program. After it opens, from there, click "add mod" to load the modded Luminescent romfs/exefs folders. Once you're finished editing, hit export to create your edited files to the Output folder, and to install replace your unedited Luminescent mod, with the now edited Luminescent files from the "Output" folder it generated for you.\n\nAlways download the latest version from the [releases page](https://github.com/Nifyr/Imposters-Ordeal/releases). Requires .NET 5.0.`,
 				);
 		else if (message.content.toLowerCase().includes("[imposters ordeal]"))
 			embed
@@ -50,7 +50,7 @@ module.exports = {
 					"https://www.serebii.net/cafemix/pokemon/132-pretendeevee.png",
 				)
 				.setDescription(
-					`Imposter's Ordeal is a BDSP randomizer that also allows you to edit a vast array of game functions, such as:\nMove attributes (types, additional effects, accuracy, etc), shop lists, route encounters, and Pokémon statistics such as their typing, BST, and wild held items, Trainer team composition and much more.\n\nIt requires a romfs/exefs extraction of BDSP 1.3.0 to initially startup the program. After it opens, from there, click "add mod" to load the modded Luminescent romfs/exefs folders. Once you're finished editing, put the resultant "output" files into the space of your Luminescent mod.\n\nAlways download the latest version from the releases page. Requires .NET 5.0.`,
+					`Imposter's Ordeal is a BDSP randomizer that also allows you to edit a vast array of game functions, such as:\nMove attributes (types, additional effects, accuracy, etc), shop lists, route encounters, and Pokémon statistics such as their typing, BST, and wild held items, Trainer team composition and much more.\n\nIt requires a romfs/exefs extraction of BDSP 1.3.0 to initially startup the program. After it opens, from there, click "add mod" to load the modded Luminescent romfs/exefs folders. Once you're finished editing, hit export to create your edited files to the Output folder, and to install replace your unedited Luminescent mod, with the now edited Luminescent files from the "Output" folder it generated for you.\n\nAlways download the latest version from the [releases page](https://github.com/Nifyr/Imposters-Ordeal/releases). Requires .NET 5.0.`,
 				);
 		else
 			embed
